4d0384539f846fee9a2793deb298fba4914b36f3
eiji <c0b23082de@edu.teu.ac.jp>
2024-06-11 17:05:42 +0900

    初コミット

--- pygame_system.py
+++ pygame_system.py
@@ -7,15 +7,15 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     pg.display.set_caption("はじめてのPygame")
-    screen = pg.display.set_mode((800, 600))
+    screen = pg.display.set_mode((1600, 600))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
 
     enn = pg.Surface((20, 20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
-
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -26,7 +26,7 @@ def main():
         screen.blit(enn, [100, 400])
         pg.display.update()
         tmr += 1        
-        clock.tick(3)
+        clock.tick()
 
 
 if __name__ == "__main__":
